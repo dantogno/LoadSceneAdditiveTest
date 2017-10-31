9a6b9e104391b424261ce9ab0cc633655bd0f208
David Antognoli <david.antognoli@gmail.com>
2017-10-31 17:49:42 -0500

    initial project add

---
+++ Assets/LoadMultipleScenes.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadMultipleScenes : MonoBehaviour 
+{
+    [SerializeField]
+    private int numberOfExtraScenesToLoad = 3;
+
+    private void Start () 
+	{
+        for (int i = 1; i <= numberOfExtraScenesToLoad; i++)
+        {
+            SceneManager.LoadSceneAsync(i, LoadSceneMode.Additive);
+        }
+	}
+}
